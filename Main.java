16e63d8ef792df6954f0d20462f54831d31858dc
santyxxx-xxx <santiago.vargas@epn.edu.ec>
2025-04-27 09:17:03 -0500

    deber

--- Main.java
+++ Main.java
@@ -2,12 +2,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        persona p1=new persona();
-        p1.nombre="santy";
-        p1.apellido="santy";
-        p1.edad=30;
-        p1.mostrar();
+        persona p1=new persona("Santy","Vargas",20);
+                p1.mostrar();
     }
 }
-//Si no creo el constructor me toca en el main escribir y llenar cada cosa,
-// lo cual si lo creo solo lleno los campos y llamo al void para que ejecute la accion
+//Si creo sin constructor toca crear los campos por mi mismo aqui en el main, pero con el contructor solo
+//lleno los campos
--- persona.java
+++ persona.java
@@ -2,7 +2,11 @@ public class persona {
     String nombre;
     String apellido;
     int edad;
-
+    public persona(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
     public void mostrar(){
         System.out.println("Nombre: " + nombre);
         System.out.println("Apellido: " + apellido);
